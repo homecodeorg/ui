ed6d55aecd30f8052df3e4006559947dce969675
Oleg Apostol <truerenton@gmail.com>
2022-03-17 09:07:52 +0100

    fix: Scroll bar position, Spinner default size=m

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,7 +3,7 @@ import cn from 'classnames';
 import SpinnerIcon from './spinner.svg';
 import S from './Spinner.styl';
 
-export const Spinner = ({ size = 's', className = '', ...props }) => {
+export const Spinner = ({ size = 'm', className = '', ...props }) => {
   const classes = cn(S.root, S[`size-${size}`], className);
 
   return <SpinnerIcon className={classes} {...props} />;
