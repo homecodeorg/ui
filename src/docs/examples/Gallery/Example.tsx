94b3f31482880d08377250c16c0952ff127d50db
Oleg Apostol <truerenton@gmail.com>
2022-03-30 21:59:39 +0200

    fix: Gallery - start at first, recenter()

--- src/docs/examples/Gallery/Example.tsx
+++ src/docs/examples/Gallery/Example.tsx
@@ -12,3 +12,9 @@ const items = [
 export default () => {
   return <Gallery items={items} style={{ height: 400 }} />;
 };
+
+// 'https://picsum.photos/id/5/400/300.jpg',
+// 'https://picsum.photos/id/432/400/300.jpg',
+// 'https://picsum.photos/id/336/400/300.jpg',
+// 'https://picsum.photos/id/210/400/300.jpg',
+// 'https://picsum.photos/id/248/400/300.jpg',
