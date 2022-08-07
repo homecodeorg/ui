9b6e0ad64a3ae706b43b1fa36bf74c9ed763464a
Oleg Apostol <truerenton@gmail.com>
2022-08-07 19:56:45 +0200

    fix: Router - Redirect fire on mount

--- src/components/Router/Redirect.tsx
+++ src/components/Router/Redirect.tsx
@@ -5,9 +5,8 @@ import STORE from './store';
 type Props = { to: string };
 
 export class Redirect extends Component<Props> {
-  constructor(props) {
-    super(props);
-    STORE.replaceState(props.to, { replace: true });
+  componentDidMount(): void {
+    STORE.replaceState(this.props.to, { replace: true });
   }
 
   render() {
