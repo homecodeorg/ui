38126f3018af2d1d484381a3a0484e5737f650d2
Oleg Apostol <truerenton@gmail.com>
2023-03-27 00:11:52 +0200

    chore: update README/About

--- README.md
+++ README.md
@@ -2,10 +2,28 @@
 
 # UILIB
 
-React UI components library
+Design system that makes building web applications with ReactJS faster and easier.
 
-> NOTE: v3 migrated to react@18
+It's a collection of pre-built components, guidelines, and best practices that help you create high-quality user interfaces with less code.
 
-- fully typed
-- tree-shaking
-- [playground](https://foreverido.github.io/uilib)
+## Best practices
+
+Codebase are folows software development principles: DRY, KISS, SOLID.
+
+### Tree shakable
+
+UILIB is designed to make your web application run faster by only including the components that are actually used in your code. This reduces the amount of code that needs to be loaded, which makes your application load faster and run more smoothly.
+
+### Typescript support
+
+UILIB is designed to work well with Typescript, which is a popular programming language that helps you write more reliable and maintainable code. With UILIB, you get all the benefits of Typescript, like better error messages and more accurate code completion, to make your development experience smoother and more productive.
+
+### SSR compatible
+
+The components can be used in projects with SSR enabled. Check the test project [uilib-next](https://github.com/foreverido/uilib-nextjs)
+
+## Perspective
+
+There's still a lot of work to be done to make it shine ✨
+
+Always welcome to contact me if you have any ideas - [Twitter](https://twitter.com/vpostol), [Telegram](https://t.me/apostol) or create issue/PR in [GitHub](https://github.com/foreverido/uilib/pulls).
--- src/docs/pages/About.tsx
+++ src/docs/pages/About.tsx
@@ -6,7 +6,16 @@ import { I18N } from '../config/i18n';
 export default function About() {
   return (
     <Page header={<I18N id="About" />}>
-      <p>UILIB is a React components library for building user interfaces.</p>
+      <p>
+        Design system that makes building web applications with ReactJS faster
+        and easier.
+      </p>
+      <p>
+        It's a collection of pre-built components, guidelines, and best
+        practices that help you create high-quality user interfaces with less
+        code.
+      </p>
+
       <H2>Best practices</H2>
       <p>
         Codebase are folows software development principles: DRY, KISS, SOLID.
@@ -14,14 +23,19 @@ export default function About() {
 
       <H3>Tree shakable</H3>
       <p>
-        It's tree-shakable. Bundled in ESM format. So your application bundle
-        will increase as much as you import from UILIB.
+        UILIB is designed to make your web application run faster by only
+        including the components that are actually used in your code. This
+        reduces the amount of code that needs to be loaded, which makes your
+        application load faster and run more smoothly.
       </p>
 
       <H3>Typescript support</H3>
       <p>
-        There are still some issues with bundling typescript files. But every
-        components/tool/helper are fully typed already.
+        UILIB is designed to work well with Typescript, which is a popular
+        programming language that helps you write more reliable and maintainable
+        code. With UILIB, you get all the benefits of Typescript, like better
+        error messages and more accurate code completion, to make your
+        development experience smoother and more productive.
       </p>
 
       <H3>SSR compatible</H3>
@@ -35,9 +49,8 @@ export default function About() {
 
       <H2>Perspective</H2>
       <p>There's still a lot of work to be done to make it shine ✨</p>
-      {/* <p>I will continue to describe it separately for each component.</p> */}
       <p>
-        Also welcome to contact me if you have any ideas -&nbsp;
+        Always welcome to contact me if you have any ideas -&nbsp;
         <Link inline href="https://twitter.com/vpostol">
           Twitter
         </Link>
@@ -45,7 +58,8 @@ export default function About() {
         <Link inline href="https://t.me/apostol">
           Telegram
         </Link>
-        .
+        &nbsp;or create issue/PR in{' '}
+        <Link href="https://github.com/foreverido/uilib/pulls">GitHub</Link>.
       </p>
     </Page>
   );
