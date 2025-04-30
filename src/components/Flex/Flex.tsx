ba25fb79cb28ccc27218db30545930a9e45c4af2
Oleg Apostol <truerenton@gmail.com>
2025-04-30 19:07:29 +0100

    fix: Flex - props as CSSProperties do not pass to DOM

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -3,27 +3,54 @@ import { CSSProperties, HTMLAttributes } from 'react';
 import S from './Flex.styl';
 
 type FlexProps = HTMLAttributes<HTMLDivElement> &
-  CSSProperties & {
+  Pick<
+    CSSProperties,
+    | 'justifyContent'
+    | 'alignItems'
+    | 'flexDirection'
+    | 'flexWrap'
+    | 'flexGrow'
+    | 'flexShrink'
+    | 'flexBasis'
+    | 'flex'
+    | 'order'
+    | 'alignSelf'
+    | 'flexFlow'
+  > & {
     children: React.ReactNode;
   };
 
-export const Flex = ({ children, ...props }: FlexProps) => {
+export const Flex = ({
+  children,
+  justifyContent,
+  alignItems,
+  flexDirection,
+  flexWrap,
+  flexGrow,
+  flexShrink,
+  flexBasis,
+  flex,
+  order,
+  alignSelf,
+  flexFlow,
+  ...props
+}: FlexProps) => {
   return (
     <div
       className={S.root}
-      {...props}
+      // {...props}
       style={{
-        justifyContent: props.justifyContent,
-        alignItems: props.alignItems,
-        flexDirection: props.flexDirection,
-        flexWrap: props.flexWrap,
-        flexGrow: props.flexGrow,
-        flexShrink: props.flexShrink,
-        flexBasis: props.flexBasis,
-        flex: props.flex,
-        order: props.order,
-        alignSelf: props.alignSelf,
-        flexFlow: props.flexFlow,
+        justifyContent,
+        alignItems,
+        flexDirection,
+        flexWrap,
+        flexGrow,
+        flexShrink,
+        flexBasis,
+        flex,
+        order,
+        alignSelf,
+        flexFlow,
       }}
     >
       {children}
