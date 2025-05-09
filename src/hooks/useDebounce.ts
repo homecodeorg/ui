fa8d32c9484d5a9e871ff27629a7e53b105ac4ae
Oleg Apostol <truerenton@gmail.com>
2025-05-09 15:11:40 +0100

    feat: useDebounceCallback() hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -5,3 +5,11 @@ import debounce from '../tools/debounce';
 export function useDebounce(fn: (...args: any[]) => void, delay: number) {
   return useCallback(debounce(fn, delay), [fn, delay]);
 }
+
+export function useDebounceCallback(
+  fn: (...args: any[]) => void,
+  delay: number,
+  deps: any[]
+) {
+  return useCallback(debounce(fn, delay), deps);
+}
