deb87e9084bad291bc995348e3d039daaa8f397b
Oleg Apostol <truerenton@gmail.com>
2025-09-07 10:54:20 +0100

    fix: Scroll - fadeSize:xl - types

--- src/components/Scroll/Scroll.types.ts
+++ src/components/Scroll/Scroll.types.ts
@@ -27,7 +27,7 @@ export type Props = ComponentType &
     // Scrollbars size.
     size?: Size;
     // Content fade size.
-    fadeSize?: Size;
+    fadeSize?: Size | 'xl';
     // Enables scroll-behavior smooth (default false).
     smooth?: boolean;
     // Whether to automatically hide the scrollbar when not in use (default false).
