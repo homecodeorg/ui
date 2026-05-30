31290f63410dd04659c2257133eb108fdc930821
Oleg Apostol <truerenton@gmail.com>
2026-05-30 19:52:29 +0100

    fix: Card styles and exmaples

--- src/docs/examples/Card/Example.tsx
+++ src/docs/examples/Card/Example.tsx
@@ -1,8 +1,13 @@
-import { Button, Card, CardContent, CardFooter, CardHeader } from 'uilib';
+import { Button, Card, CardContent, CardFooter, CardHeader, Icon } from 'uilib';
 
 export default () => (
-  <Card paddingSize="m" blur>
-    <CardHeader title="Card title" description="Supporting description text." />
+  <Card paddingSize="m" blur >
+    <CardHeader
+      title="Card title"
+      description="Supporting description text."
+      icon={<Icon type="chat" size="l" />}
+      iconWithBackground
+    />
     <CardContent>
       Card content area. Use Card as a layout shell for panels, lists, or forms.
     </CardContent>
